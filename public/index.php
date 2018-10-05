6f5ca0897eb132858f27cdb4a33f770023e13753
sagarshah4819 <ss3958@njit.edu>
2018-10-05 16:43:17 -0700

    Task1: Created a main class in-order to read a csv file.

--- public/index.php
+++ public/index.php
@@ -1,2 +1,19 @@
 <?php
-echo "Welcome to NJIT";
+
+main::start();
+
+class main {
+
+    static public function start()
+    {
+
+        $file = fopen("TestFile.csv", "r");
+
+        while (!feof($file)) {
+
+            print_r(fgetcsv($file));
+
+        }
+    fclose($file);
+    }
+}
